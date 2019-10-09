2892a0d3429d6b3fc42d7e518ea2cd06485c720b
Fabio Oliveira <silvadeoliveira@gmail.com>
2019-10-08 21:59:09 -0300

    ajuste Domain IRepository IContaCorrente.cs

---
+++ src/Superdigital.Domain/IRepository/AggregateConta/IContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿using Superdigital.CoreShared.Data;
+using Superdigital.Domain.Entities.AggregateConta;
+
+namespace Superdigital.Domain.IRepository.AggregateConta
+{
+    public interface IContaCorrente : IRepository<ContaCorrente>
+    {
+        
+    }
+}
